e2d7894ec76308cc5e3730cb3443c7a8ea7cdd5d
JKyEun <j56237@naver.com>
2023-07-01 02:03:43 +0900

    rename: redirect uri 수정

--- src/util/constant.ts
+++ src/util/constant.ts
@@ -2,7 +2,7 @@ export const GRANT_TYPE = 'authorization_code';
 export const KAKAO_CLIENT_ID = process.env.REACT_APP_KAKAO_CLIENT_ID;
 export const KAKAO_REDIRECT_URI = window.location.origin.includes('localhost')
   ? 'http://localhost:3000/oauth/callback/kakao'
-  : 'http://43.201.17.248:3000/oauth/callback/kakao';
+  : 'http://headfirst.moim.link/:3000/oauth/callback/kakao';
 export const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${KAKAO_CLIENT_ID}&redirect_uri=${KAKAO_REDIRECT_URI}&response_type=code`;
 
 export const companyClass = ['사기업', '공기업', '스타트업', '외국계', '기타'];
