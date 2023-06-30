ae489ad8945e33cf752cce875226a7cf17742b18
JKyEun <j56237@naver.com>
2023-07-01 01:47:08 +0900

    rename: console.log 삭제

--- src/pages/MentorRegisterPage.tsx
+++ src/pages/MentorRegisterPage.tsx
@@ -36,30 +36,17 @@ export default function MentorRegisterPage() {
 
   const submitInfo = async () => {
     try {
-      const res1 = await axios.post('http://43.201.17.248:8080/role', null, {
+      await axios.post('http://43.201.17.248:8080/role', null, {
         headers: {
           Authorization: `Bearer ${localStorage.getItem('JWT_TOKEN')}`,
         },
       });
-      console.log(res1.data);
-
-      console.log(info);
 
       const res = await axios.post('http://43.201.17.248:8080/mentor/info', info, {
         headers: {
           Authorization: `Bearer ${localStorage.getItem('JWT_TOKEN')}`,
         },
       });
-
-      // const res = await fetch('http://43.201.17.248:8080/mentor/info', {
-      //   method: 'POST',
-      //   headers: {
-      //     Authorization: `Bearer ${localStorage.getItem('JWT_TOKEN')}`,
-      //     'Content-type': 'application/json',
-      //   },
-      //   body: JSON.stringify(info),
-      // });
-      console.log(await res.data);
 
       if (res.status === 200) {
         navigate('/');
